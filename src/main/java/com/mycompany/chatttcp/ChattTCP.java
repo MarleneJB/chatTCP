9894f53d3d48ece9347f0dd260f4c49209e803c4
MarleneJB <marlen10224@gmail.com>
2024-03-04 11:37:39 -0600

    inicio

---
+++ src/main/java/com/mycompany/chatttcp/ChattTCP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chatttcp;
+
+/**
+ *
+ * @author marle
+ */
+public class ChattTCP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
